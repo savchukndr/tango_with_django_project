ed880c116111cdd7d310ae180d14bf016d63d4a3
Andrii Savchuk <savchukndr@gmail.com>
2016-09-09 20:38:45 +0300

    added about.html

--- rango/views.py
+++ rango/views.py
@@ -8,4 +8,6 @@ def index(request):
     return render_to_response('rango/index.html', context_dict, context)
 
 def about(request):
-    return HttpResponse("Rango says: Here is the about page.")
+    context = RequestContext(request)
+    context_dict = {'boldmessage1': "I am bold font from the context for ABOUT"}
+    return render_to_response('rango/about.html', context_dict, context)
--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'rango',
 ]
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -123,3 +125,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
